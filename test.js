a32b4bafbc4718ac55b87dd726c7ec5cbb0870db
Nutto456 <s6506022610022@email.kmutnb.ac.th>
2023-09-26 11:09:43 +0700

    update

---
+++ test.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect(
+    'mongodb://admin:IDKxqf89775@10.104.12.254:27017', 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    }
+);
+
+const Book = mongoose.model('Book', {
+    id: Number,
+    title: String,
+    author: String
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/books',async (req, res) => {
+    try {
+        const book = new Book(req.body);
+        book.id = (await Book.countDocuments()) + 1;
+        await book.save();
+        res.send(book);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.send(books);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const book = await Book.findOne({ id: req.params.id });
+        res.send(book);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.put('/books/:id', async (req, res) => {
+    try {
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true });
+        res.send(book);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+        res.send(book);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
